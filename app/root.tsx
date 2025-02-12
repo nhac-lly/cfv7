24b2824ef5ab68ac8499bc19a08f646ad8a31990
nhac <nhac.ly@hotmail.com>
2025-02-12 14:53:18 +0700

    add back style

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { useState } from "react";
 import type { Route } from "./+types/root";
+import stylesheet from "./app.css?url";
 
 export const links: Route.LinksFunction = () => [
  { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -26,6 +27,7 @@ export const links: Route.LinksFunction = () => [
   href:
    "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
  },
+ { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
